2231a2dadb5c212a280a72d3c4785e60da277d74
trusz <trusz@tuta.io>
2021-09-26 17:20:07 +0200

    feat: add base FE

---
+++ frontend/src/routes/create/index.svelte
@@ -0,0 +1,11 @@
+
+<label for="room-name">Room Name</label>
+<input id="room-name" type="text" placeholder="random room name" />
+
+
+<label for="select-deck">Select Deck</label>
+<select id="select-deck">
+    <option value="" selected></option>
+    <option value="simple">Simple</option>
+    <option value="fibonacchi">Fibonacchi</option>
+</select>
---
+++ frontend/src/routes/join/index.svelte
@@ -0,0 +1,3 @@
+
+<label for="room-name">Room Name</label>
+<input id="room-name" type="text" placeholder="random room name" />
