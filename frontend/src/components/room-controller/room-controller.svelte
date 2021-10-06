6b58924a7cc64727fb9fdbaedf24b83de178a241
trusz <trusz@tuta.io>
2021-10-06 08:03:14 +0200

    feat: topic

--- frontend/src/components/room-controller/room-controller.svelte
+++ frontend/src/components/room-controller/room-controller.svelte
@@ -19,27 +19,51 @@ import type { Player } from "../player-list/player";
     }
 
 </script>
+<room-controller>
+    {#if status === Status.Start}
+        <button on:click={onStartClick}> Start Voting</button>
+    {/if}
 
-<div class="room-controller-root">
-{#if status === Status.Start}
-    <button on:click={onStartClick}> Start Voting</button>
-{/if}
+    {#if status === Status.InProgress}
+        <button on:click={onRevealClick} disabled={player.chosenCard === ""}> Reveal Cards</button>
+    {/if}
+    
+    {#if status === Status.Revealed}
+        <button on:click={onResetClick}> Reset </button>
+    {/if}
 
-{#if status === Status.InProgress}
-    <button on:click={onRevealClick} disabled={player.chosenCard === ""}> Reveal Cards</button>
-{/if}
- 
-{#if status === Status.Revealed}
-    <button on:click={onResetClick}> Reset </button>
-{/if}
-</div>
+
+    <div>
+        <label for="topic">Topic</label>
+        <input id="topic" type="text" />
+    </div>
+
+</room-controller>
 
 <style>
 
-    .room-controller-root{
+    room-controller {
+        background:      transparent;
+        /* border:          1px white solid; */
         display:         flex;
+        /* padding:         1rem; */
+        width:           auto;
+        gap:             2rem;
         justify-content: center;
+        
+        --shadow-color: rgb(209, 209, 209);
+        --shadow: 1px 0px 3px 1px var(--shadow-color), inset 0px 0px 3px 1px var(--shadow-color); ; 
+
+        outline:            none;
+        /* -webkit-box-shadow: var(--shadow); */
+        /* box-shadow: 		var(--shadow); */
     }
+/* 
+    room-controller:focus,
+    room-controller:focus-within,
+    room-controller:active {
+        
+    } */
 
     button {
         width: 10rem;
